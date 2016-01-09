66b2f72a0f71fe227dfb1ab08e9a1b0b384dcdb6
tglo <tglo@tglo-PC>
2016-01-09 15:27:49 +0100

    First Commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: tglo
+ *
+ * Created on 9 janvier 2016, 15:12
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    #include <stdio.h>
+#include <stdlib.h>
+
+    return 0;
+}
+
